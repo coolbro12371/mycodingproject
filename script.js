d9274dc07ccd0f13aabbe54ddfb1b3017ff739d6
Caleb <82981948+coolbro12371@users.noreply.github.com>
2024-10-31 16:49:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// your code goes here
+let valpp = document.getElementById("input1");
+let addTitle2 = document.getElementById("yourTitle");
+function addTitle() {
+    if(valpp.value === "Password"){
+        addTitle2.textContent = "Correct Password";
+        alert("Correct");
+        document.body.style.background = "green";
+        document.getElementById("button1").onclick = function (){
+    const val1 = valpp.value;
+    if(addTitle2.textContent === "Correct Password"){
+        alert("Error 404: Same Password");
+        addTitle2.textContent = "Error 404: Same Password";
+        setTimeout(redoTitle,3000);
+    }
+};
+    }else if(valpp.value === "999"){
+        document.body.style.background = "purple";
+        addTitle2.textContent = "999FL & DRFL";
+    }else {
+        alert("Password Incorrect.. Please Reload Page To Try Again");
+        document.body.style.background = "red";
+    }
+}
+function redoTitle(){
+    addTitle2.textContent = "Correct Password";
+    document.getElementById("button1").onclick = function (){ 
+        if(valpp.value === "Password"){
+        addTitle2.textContent = "Correct Password";
+        alert("Error 404: Same Password");
+        document.body.style.background = "green";
+        document.getElementById("button1").onclick = function (){
+    const val1 = valpp.value;
+    if(addTitle2.textContent === "Correct Password"){
+        alert("Error 404: Same Password");
+        addTitle2.textContent = "Error 404: Same Password";
+        setTimeout(redoTitle,3000);
+    }
+};
+}
+};
+}
+function resetall(){
+    const val1 = valpp.value;
+    addTitle2.textContent = "Enter The Correct Password";
+    document.body.style.background = "none";
+    valpp.value = "";
+}
